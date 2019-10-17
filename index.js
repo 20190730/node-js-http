c92e02510406e89acd005fbf438d3cf61d97eb88
20190730 <ifukukaitai2019+20190730@gmail.com>
2019-10-17 20:26:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+const http = require('http');
+const pug = require('pug');
+const server = http.createServer((req, res) => {
+  const now = new Date();
+  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
+
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/enquetes/yaki-shabu'){
+        res.write(pug.renderFile('./form.pug',{
+          path: req.url,
+          firstItem:'焼き肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
+      }else if (req.url === '/enquetes/rice-bread'){
+        res.write(pug.renderFile('./form.pug',{
+          path: req.url,
+          firstItem:'ごはん',
+          secondItem: 'パン'
+        }));
+      }
+        res.end();
+      break;
+    case 'POST':
+      let body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        const decoded = decodeURIComponent(body);
+        console.info('[' + now + '] 投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+          decoded + 'が投稿されました</h1></body></html>');
+        res.end();
+      });
+      break;
+    default:
+      break;
+  }
+
+}).on('error', (e) => {
+  console.error('[' + new Date() + '] Server Error', e);
+}).on('clientError', (e) => {
+  console.error('[' + new Date() + '] Client Error', e);
+});
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.info('[' + new Date() + '] Listening on ' + port);
+});
